3c32c58b92382c199dc07f2a8183a4435a9836ff
robbie <dubiousLogik@gmail.com>
2015-06-03 19:39:37 -0700

    Converters workbook
    Reorganizing folder structure to better group the code

---
+++ Converters/BinaryConverter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryAndHexWorkbook
+{
+    public static class BinaryConverter
+    {
+        public static bool TryParseStringToInt32(string input, out int output)
+        {
+            if (Validate(input))
+            {
+                output = ToInt32(input);
+                return true;
+            }
+            else
+            {
+                output = -1;
+                return false;
+            }
+        }
+
+        static bool Validate(string input)
+        {
+            bool isValidInput = true;
+            char[] digits = input.ToCharArray();
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if ((digits[i] >= '0') && (digits[i] <= '1'))
+                {
+                    //Ok
+                }
+                else
+                {
+                    isValidInput = false;
+                }
+
+            }
+
+            return isValidInput;
+        }
+
+        static int ToInt32(string input)
+        {
+            int output = 0;
+            char[] digits = input.ToCharArray();
+            int power = 0;
+            int base2 = 2;
+
+            for (int i = digits.Length-1; i >=0 ; i--)
+            {
+                int thisDigit = digits[i] - '0';
+                double d = Math.Pow(Convert.ToDouble(base2), Convert.ToDouble(power));
+                output += Convert.ToInt32(d) * thisDigit;
+                power++;
+            }
+            return output;
+        }
+    }
+}
---
+++ Converters/HexConverter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryAndHexWorkbook
+{
+    public static class HexConverter
+    {
+        public static bool TryParseToInt32(string input, out int output)
+        {
+            if (Validate(input))
+            {
+                output = ToInt32(input);
+                return true;
+            }
+            else
+            {
+                output = -1;
+                return false;
+            }
+        }
+
+        static bool Validate(string input)
+        {
+            bool isValidHexNumber = true;
+            char[] digits = input.ToCharArray();
+
+            for (int i=0; i<input.Length; i++)
+            {
+                if (((digits[i] >= '0') && (digits[i] <= '9')) || ((digits[i] >= 'A') && (digits[i] <= 'F')) || ((digits[i] >= 'a') && (digits[i] <= 'f')))
+                {
+                    //Ok
+                }
+                else
+                {
+                    isValidHexNumber = false;
+                }
+            }
+            return isValidHexNumber;
+        }
+
+        static int ToInt32(string input)
+        {
+            int returnNumber = 0;
+            int thisDigit = 0;
+            int power = 0;
+            int base16 = 16;
+
+            char[] digits = input.ToCharArray();
+            
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                //Parse using char distance approach
+                char c = digits[i];
+                if ((c >= '0') && (c <= '9'))
+                {
+                    thisDigit = c - '0';
+                }
+                if ((c >= 'A') && (c <= 'F'))
+                {
+                    thisDigit = c + 10 - 'A';
+                }
+                if ((c >= 'a') && (c <= 'f'))
+                {
+                    thisDigit = c + 10 - 'a';
+                }
+                double d = Math.Pow(Convert.ToDouble(base16), Convert.ToDouble(power));
+                returnNumber += Convert.ToInt32(d) * thisDigit;                
+                power++;
+            }
+            return returnNumber;
+        }
+
+    }
+}
