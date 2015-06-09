50768a189496a6c2cabbf3d5eaea2c9ece1eff31
robbie <dubiousLogik@gmail.com>
2015-06-09 00:17:47 -0700

    Lists Workbook
    added GetFirstNode, GetNextNode, and removed debug code that outputs to
    console

--- Lists/SinglyLinkedList.cs
+++ Lists/SinglyLinkedList.cs
@@ -9,7 +9,8 @@ namespace Lists
     /* ************************************************
      * SinglyLinkedList.cs
      * 
-     * Purpose:  Implement a singly linked list with ability to add nodes and return the output of the list
+     * Purpose:  Implement a singly linked list with ability to add nodes, get nodes, and return the 
+     *   values of all nodes as a string
      * 
      * Goal:  Learn how to create linked lists, specifically the 'next node' pointer.  In C# I'm not 
      *   explicitly manipulating pointers, however the NextNode object contained within the current Node
@@ -18,9 +19,13 @@ namespace Lists
      *   
      * Design Choices:  I chose to separate the SinglyLinkedList class from the underlying Node implementation.
      *   The intent is that the user could interact with the SinglyLinkedList class and not need to know about
-     *   the Node class.  I added the lastNode element as a perf optimization, so that you don't re-traverse
+     *   the Node class.  Only the Node class can set the NextNode element, this enforces that the user 
+     *   cannot make an error here.
+     * 
+     * Performance:  I added the lastNode element as a perf optimization, so that you don't re-traverse
      *   the entire list every time you add an element to the end, which matters a lot as the list gets large.
-     *   With this optimization it becomes a viable StringBuilder as well.
+     *   With this optimization it becomes a viable as an alternate implementation of StringBuilder that does
+     *   not use Arrays.
      *   
      * Author:  Robbie Devine, 08 Jun 2015  
      * ************************************************
@@ -47,20 +52,14 @@ namespace Lists
             }
         }
 
-        public void OutputNodeValuesToConsole()
+        public Node GetFirstNode() 
         {
-            if (firstNode == null) 
-            { 
-                return; 
-            }
+            return firstNode;
+        }
 
-            Node thisNode = firstNode;
-            Console.WriteLine("Current Node data payload : {0}", thisNode.Data);
-            while (thisNode.NextNode != null)
-            {
-                thisNode = thisNode.NextNode;
-                Console.WriteLine("Current Node data payload : {0}", thisNode.Data);
-            }
+        public Node GetNextNode(Node currentNode)
+        {
+            return currentNode.NextNode;
         }
 
         public string OutputNodeValuesToString()
