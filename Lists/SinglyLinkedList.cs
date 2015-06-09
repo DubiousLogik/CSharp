705c3c6557c8dd703a415bae1054264c848c91fe
robbie <dubiousLogik@gmail.com>
2015-06-08 22:45:32 -0700

    Lists Workbook
    adding SinglyLinkedList class and unit tests

---
+++ Lists/SinglyLinkedList.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public class SinglyLinkedList
+    {
+        Node firstNode = null;
+
+        public SinglyLinkedList() {}
+
+        public void AddNode(string input) 
+        {
+            if (firstNode == null)
+            {
+                firstNode = new Node(input);
+            }
+            else
+            {
+                firstNode.AddToEnd(new Node(input));
+            }
+        }
+
+        public void OutputNodeValuesToConsole()
+        {
+            if (firstNode == null) 
+            { 
+                return; 
+            }
+
+            Node thisNode = firstNode;
+            Console.WriteLine("Current Node data payload : {0}", thisNode.Data);
+            while (thisNode.nextNode != null)
+            {
+                thisNode = thisNode.nextNode;
+                Console.WriteLine("Current Node data payload : {0}", thisNode.Data);
+            }
+        }
+
+        public string OutputNodeValuesToString()
+        {
+            if (firstNode == null)
+            {
+                return "";
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            Node thisNode = firstNode;
+            result.Append(thisNode.Data);
+
+            while (thisNode.nextNode != null)
+            {
+                thisNode = thisNode.nextNode;
+                result.Append(thisNode.Data);
+            }
+
+            return result.ToString();
+        }
+    }
+
+    public class Node
+    {
+        public Node nextNode { get; private set; }
+        public string Data { get; private set; }
+
+        public Node(string input)
+        {
+            Data = input;
+        }
+
+        public void AddToEnd(Node newNode)
+        {
+            Node thisNode = this;
+            while (thisNode.nextNode != null)
+            {
+                thisNode = thisNode.nextNode;
+            }
+            thisNode.nextNode = newNode;
+        }
+    }
+}
---
+++ Lists/SinglyLinkedListTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lists
+{
+    [TestClass]
+    public class SinglyLinkedListTests
+    {
+        [TestMethod]
+        public void SinglyLinkedListTest1()
+        {
+            string expectedListValues = "";
+            SinglyLinkedList list = new SinglyLinkedList();
+
+            string actualListValues = list.OutputNodeValuesToString();
+
+            Assert.AreEqual(expectedListValues, actualListValues);
+        }
+
+        [TestMethod]
+        public void SinglyLinkedListTest2()
+        {
+            string expectedListValues = "0";
+            SinglyLinkedList list = new SinglyLinkedList();
+
+            list.AddNode("0");
+
+            string actualListValues = list.OutputNodeValuesToString();
+
+            Assert.AreEqual(expectedListValues, actualListValues);
+        }
+
+        [TestMethod]
+        public void SinglyLinkedListTest3()
+        {
+            string expectedListValues = "0123456789";
+            SinglyLinkedList list = new SinglyLinkedList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddNode(i.ToString());
+            }
+
+            string actualListValues = list.OutputNodeValuesToString();
+
+            Assert.AreEqual(expectedListValues, actualListValues);
+        }
+
+        [TestMethod]
+        public void SinglyLinkedListTest4()
+        {
+            string expectedListValues = "The man in the moon likes gouda.";
+            SinglyLinkedList list = new SinglyLinkedList();
+
+            list.AddNode("The ");
+            list.AddNode("man ");
+            list.AddNode("in ");
+            list.AddNode("the ");
+            list.AddNode("moon ");
+            list.AddNode("likes ");
+            list.AddNode("gouda.");
+
+            string actualListValues = list.OutputNodeValuesToString();
+
+            Assert.AreEqual(expectedListValues, actualListValues);
+        }
+    }
+}
