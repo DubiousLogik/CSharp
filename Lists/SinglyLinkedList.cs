c85f33aac55ceb5835d6f0f2c51a6297330d3204
robbie <dubiousLogik@gmail.com>
2015-06-15 20:49:57 -0700

    Lists Workbook
    updated comments with lessons learned

--- Lists/SinglyLinkedList.cs
+++ Lists/SinglyLinkedList.cs
@@ -19,12 +19,21 @@ namespace Lists
      *   
      * Design Choices:  I chose to separate the SinglyLinkedList class from the underlying Node implementation.
      *   The intent is that the user could interact with the SinglyLinkedList class and not need to know about
-     *   the Node class.  Only the Node class can set the NextNode and Data elements.
+     *   the Node class.  Only the Node class can set the NextNode and Data elements.    
      * 
      * Performance:  I added the lastNode element as a perf optimization, so that you don't re-traverse
      *   the entire list every time you add an element to the end, which matters a lot as the list gets large.
      *   With this optimization it becomes a viable as an alternate implementation of StringBuilder that does
      *   not use Arrays.
+     *   
+     * Lessons Learned:  I began with the intent of keeping Node private, however in practice I needed to expose 
+     *   it to the end user since the paradigm for list traversal is to get the next node with each call, and 
+     *   node==null means end of list (or empty list).  I considered making a private Node class and returning
+     *   the Node.Data (whatever you were storing, in this case it's hardcoded to string).  However I realized 
+     *   the GetFirst/GetNext methods that return the next node no longer make sense - I'd be returning the next 
+     *   object value, rather than the next location in the list.  For this paradigm I think the .Net IEnumerable
+     *   approach of MoveNext and Current are more appropriate, so I will leave this code as-is and use the  
+     *   IEnumerable paradigm in future coding exercises.
      *   
      * Author:  Robbie Devine, 08 Jun 2015  
      * ************************************************
@@ -75,6 +84,20 @@ namespace Lists
         public bool IsEndOfList()
         {
             return (currentNode == lastNode);
+        }
+
+        public string GetNext()
+        {
+            currentNode = currentNode.NextNode;
+            if (currentNode != null)
+            {
+                return currentNode.Data;
+            }
+            else
+            {
+                return null;
+            }
+            
         }
 
         /// <summary>
