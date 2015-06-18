3f6cb603fd9bb9a1c7e9e6b5c6e3ced7d125d589
robbie <dubiousLogik@gmail.com>
2015-06-17 22:53:09 -0700

    Lists Workbook
    adding classes for SimpleStack and its test cases

---
+++ Lists/SimpleStack.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class SimpleStack<T>
+    {
+        /* ************************************************
+         * SimpleStack.cs
+         * 
+         * Purpose:  Implement a functioning stack with Push, Pop and Peek methods for generic payloads.
+         * 
+         * Goal:  Learn how to implement a stack, and to learn C# Generics. 
+         *   
+         * Design Choices:  I chose to implement this with a linked list approach, where each node on the 
+         *   stack is aware of its previous node.  The node at the bottom of the stack (the first one
+         *   added) will have a previous node == null.
+         *   
+         * Author:  Robbie Devine, 17 Jun 2015  
+         * ************************************************
+        */
+        private StackNode<T> topOfStack;
+        public bool IsEmpty;
+
+        public SimpleStack() {}
+
+        public void Push(T data)
+        {
+            StackNode<T> newNode = new StackNode<T>(data);
+            if (topOfStack == null)
+            {
+                newNode.PreviousNode = null;
+                topOfStack = newNode;
+                IsEmpty = false;
+            }
+            else
+            {
+                newNode.PreviousNode = topOfStack;
+                topOfStack = newNode;
+            }
+        }
+
+        public T Pop()
+        {
+            if (topOfStack != null)
+            {
+                T result = topOfStack.Payload;
+                topOfStack = topOfStack.PreviousNode;  //this will set topOfStack == null when popping last node
+                if (topOfStack == null)
+                {
+                    IsEmpty = true;
+                }
+                return result;
+            }
+            else
+            {
+                IsEmpty = true;
+                return default(T);
+            }
+        }
+
+        public T Peek()
+        {
+            if (topOfStack != null)
+            {
+                return topOfStack.Payload;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        private class StackNode<T>
+        {
+            public StackNode<T> PreviousNode { get; set; }
+            public T Payload { get; private set; }
+
+            public StackNode(T data)
+            {
+                Payload = data;
+            }
+        }
+    }
+}
---
+++ Lists/SimpleStackTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Lists
+{
+    [TestClass]
+    public class SimpleStackTests
+    {
+        [TestMethod]
+        public void SimpleStackTest1()
+        {
+            string expectedResult = null;
+            bool expectedEmpty = true;
+
+            SimpleStack<string> s = new SimpleStack<string>();
+            string actualResult = s.Pop();
+            bool actualEmpty = s.IsEmpty;
+
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(expectedEmpty, actualEmpty);
+        }
+
+        [TestMethod]
+        public void SimpleStackTest2()
+        {
+            int expectedResult = 0;
+            bool expectedEmpty = true;
+
+            SimpleStack<int> s = new SimpleStack<int>();
+            int actualResult = s.Pop();
+            bool actualEmpty = s.IsEmpty;
+
+            Assert.AreEqual(expectedResult, actualResult);
+            Assert.AreEqual(expectedEmpty, actualEmpty);
+        }
+
+        [TestMethod]
+        public void SimpleStackTest3()
+        {
+            string expectedResult = "9876543210";
+
+            SimpleStack<int> s = new SimpleStack<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                s.Push(i);
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while (!s.IsEmpty)
+            {
+                sb.Append(s.Pop().ToString());
+            }
+            string actualResult = sb.ToString();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SimpleStackTest4()
+        {
+            string expectedResult = "02468 97531";
+
+            SimpleStack<int> s = new SimpleStack<int>();
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < 10; i++)
+            {
+                s.Push(i);
+                if ((i % 2) == 0)
+                {
+                    sb.Append(s.Pop().ToString());
+                }
+            }
+
+            sb.Append(" ");
+
+            while (!s.IsEmpty)
+            {
+                sb.Append(s.Pop().ToString());
+            }
+            string actualResult = sb.ToString();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
