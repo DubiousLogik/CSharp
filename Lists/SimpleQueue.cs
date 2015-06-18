0490c18e47cd7c7bc092ea654dc04dc7e9dda33d
robbie <dubiousLogik@gmail.com>
2015-06-18 00:04:01 -0700

    Lists Workbook
    Adding SimpleQueue class and test class

---
+++ Lists/SimpleQueue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    /* ************************************************
+     * SimpleQueue.cs
+     * 
+     * Purpose:  Implement a functioning queue (FIFO) with Enqueue and Dequeue methods for generic payloads.
+     * 
+     * Goal:  Learn how to implement a queue capabilities, and to learn C# Generics. 
+     *   
+     * Design Choices:  I chose to implement this with a singly linked list approach.  This pattern is
+     *   the same as a regular linked list except that you don't offer the ability to traverse the whole
+     *   list, there is only Enqueue (add to end) and Dequeue (remove from front).  
+     *   
+     * Author:  Robbie Devine, 17 Jun 2015  
+     * ************************************************
+    */
+    public class SimpleQueue<T>
+    {
+        public bool IsEmpty { get; private set; }
+
+        private QueueNode<T> FirstNode;
+        private QueueNode<T> LastNode;
+
+        public SimpleQueue()
+        {
+            IsEmpty = true;
+            FirstNode = null;
+            LastNode = null;
+        }
+
+        public void Enqueue(T data)
+        {
+            QueueNode<T> newNode = new QueueNode<T>(data);
+            newNode.NextNode = null;  //adding to the end
+
+            if (IsEmpty)
+            {
+                FirstNode = newNode;
+                LastNode = FirstNode;
+                IsEmpty = false;
+            }
+            else
+            {
+                LastNode.NextNode = newNode;
+                LastNode = newNode;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+            {
+                return default(T);
+            }
+            else
+            {
+                T result = FirstNode.Payload;
+                RemoveFirstNode();
+                return result;
+            }
+        }
+
+        public void RemoveFirstNode()
+        {
+            FirstNode = FirstNode.NextNode;
+            if (FirstNode == null)
+            {
+                IsEmpty = true;
+            }
+        }
+
+        private class QueueNode<T>
+        {
+            public T Payload { get; private set; }
+            public QueueNode<T> NextNode { get; set; }
+
+            public QueueNode(T data)
+            {
+                Payload = data;
+            }
+
+        }
+    }
+}
---
+++ Lists/SimpleQueueTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Lists
+{
+    [TestClass]
+    public class SimpleQueueTests
+    {
+        [TestMethod]
+        public void SimpleQueueTest1()
+        {
+            string expectedResult = null;
+
+            SimpleQueue<string> q = new SimpleQueue<string>();
+            
+            string actualResult = q.Dequeue();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SimpleQueueTest2()
+        {
+            string expectedResult = "one,two";
+
+            SimpleQueue<string> q = new SimpleQueue<string>();
+            q.Enqueue("one");
+            q.Enqueue("two");
+
+            string actualResult = q.Dequeue() + "," + q.Dequeue();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void SimpleQueueTest3()
+        {
+            string expectedResult = "0123456789";
+
+            SimpleQueue<int> q = new SimpleQueue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                q.Enqueue(i);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (!q.IsEmpty)
+            {
+                sb.Append(q.Dequeue().ToString());
+            }
+
+            string actualResult = sb.ToString();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
