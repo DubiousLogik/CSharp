15525ed13f4332309b9cbe24c75e1c787e6bbe91
robbie <dubiousLogik@gmail.com>
2016-06-11 19:14:30 -0700

    Puzzles
    Directory for code solutions to puzzles

---
+++ Puzzles/ReturnMaxNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzles
+{
+    class ReturnMaxNumber
+    {
+        //This class returns the max of 2 integers without using if-else, == nor comparisons of any kind
+
+        public static int ReturnMaxInt(int a, int b)
+        {
+            //d = distance from a to b
+            int d = a - b;
+            int UnitDistanceVector=0;
+            int result;
+
+            try
+            {
+                UnitDistanceVector =  (d / Math.Abs(d));  //this will fail if they are equal, otherwise will resolve to 1 or -1
+            } 
+            catch(Exception e) 
+            {
+                result = a;
+            } 
+
+            try
+            {
+                //this will fail if the UnitDistanceVector is negative, which would mean b is bigger than a
+                int[] canary = new int[UnitDistanceVector];
+                //if you get here successfully then a is bigger than b
+                result = a;
+            }
+            catch(Exception e)
+            {
+                //since we are in here then b is bigger than a
+                result = b;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Puzzles/ReturnMaxNumberTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Puzzles
+{
+    [TestClass]
+    public class ReturnMaxNumberTest
+    {
+        [TestMethod]
+        public void Permutation01()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(3, 4);
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation02()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(6, 4);
+            int expectedResult = 6;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation03()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(-2, 4);
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation04()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(4,-2);
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation05()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(-4, 2);
+            int expectedResult = 2;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation06()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(2,-4);
+            int expectedResult = 2;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation07()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(-2, -4);
+            int expectedResult = -2;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation08()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(-4, -2);
+            int expectedResult = -2;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation09()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(-4, -4);
+            int expectedResult = -4;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation10()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(4, 4);
+            int expectedResult = 4;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Permutation11()
+        {
+            int actualResult = ReturnMaxNumber.ReturnMaxInt(0, 0);
+            int expectedResult = 0;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
