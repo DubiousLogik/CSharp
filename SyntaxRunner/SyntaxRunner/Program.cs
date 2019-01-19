f5fae41c568f04eb5ff2e7747e72b0577129e7fc
robbie <dubiousLogik@gmail.com>
2019-01-19 14:18:01 -0800

    initial checkin for sytax runner !

---
+++ SyntaxRunner/SyntaxRunner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SyntaxRunner.CodeRunners;
+
+namespace SyntaxRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerfRunner.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SyntaxRunner/SyntaxRunner/String/StringCommon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SyntaxRunner.String
+{
+    public static class StringCommon
+    {
+        public static readonly Regex EmailRegex = new Regex(@"(\.|-|_|#|[a-z]|[A-Z]|[0-9])+@(\.|-|_|#|[a-z]|[A-Z]|[0-9])+", RegexOptions.Compiled);
+    }
+}
